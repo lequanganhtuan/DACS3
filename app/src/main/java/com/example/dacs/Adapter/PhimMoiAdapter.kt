fa9661ddc100889be39f0f8e8de6db0de71d4128
lequanganhtuan <lequanganhtuan.2003@gmail.com>
2023-04-09 10:40:54 +0700

    fc wibu2

---
+++ app/src/main/java/com/example/dacs/Adapter/PhimMoiAdapter.kt
@@ -0,0 +1,6 @@
+package com.example.dacs.Adapter
+
+import com.example.dacs.DataModel
+
+class PhimMoiAdapter (var pmlist : List<DataModel> ) {
+}
---
+++ app/src/main/java/com/example/dacs/DataModel.kt
@@ -0,0 +1,10 @@
+package com.example.dacs
+
+class DataModel (
+    val Ncast:  String,
+    val Ndes: String ,
+    val Nlink: String ,
+    val Nthumb : String,
+    val Ntitle : String,
+    val Tlink: String) {
+}
