a5879f99639dd26131bef2b2010e8a3b42c69b65
Wako <97005647+wako1203@users.noreply.github.com>
2023-05-05 16:05:14 +0700

    fix lỗi click listener

--- app/src/main/java/com/example/dacs/Adapter/CommentAdapter.kt
+++ app/src/main/java/com/example/dacs/Adapter/CommentAdapter.kt
@@ -3,12 +3,13 @@ package com.example.dacs.Adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dacs.Data.Comment
 import com.example.dacs.R
 
-class CommentAdapter(private val comments: List<Comment>) : RecyclerView.Adapter<CommentAdapter.ViewHolder>() {
+class CommentAdapter(private val comments: List<Comment>, private val currentUserId: String) : RecyclerView.Adapter<CommentAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.comment_card, parent, false)
@@ -17,7 +18,7 @@ class CommentAdapter(private val comments: List<Comment>) : RecyclerView.Adapter
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val cmt = comments[position]
-        holder.bind(cmt)
+        holder.bind(cmt, currentUserId)
     }
 
     override fun getItemCount(): Int = comments.size
@@ -25,9 +26,36 @@ class CommentAdapter(private val comments: List<Comment>) : RecyclerView.Adapter
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val name: TextView = itemView.findViewById(R.id.name)
         private val comment: TextView = itemView.findViewById(R.id.comment)
-        fun bind(cmt: Comment) {
+        private val option: TextView = itemView.findViewById(R.id.option)
+        fun bind(cmt: Comment, currentUserId: String) {
             name.text = cmt.nameUser
             comment.text = cmt.comment
+            if (cmt.iduser == currentUserId) {
+
+                option.visibility = View.VISIBLE
+            }
+            else
+            {
+                option.visibility = View.GONE
+            }
+            option.setOnClickListener {
+                val popupMenu = PopupMenu(this.itemView.context, option)
+                popupMenu.menuInflater.inflate(R.menu.menu_option, popupMenu.menu)
+                popupMenu.setOnMenuItemClickListener { menuItem ->
+                    when (menuItem.itemId) {
+                        R.id.edit -> {
+                            // Xử lý khi click vào menu Chỉnh sửa
+                            true
+                        }
+                        R.id.delete -> {
+                            // Xử lý khi click vào menu Xóa
+                            true
+                        }
+                        else -> false
+                    }
+                }
+                popupMenu.show()
+            }
         }
     }
 }
--- app/src/main/java/com/example/dacs/WatchMovie.kt
+++ app/src/main/java/com/example/dacs/WatchMovie.kt
@@ -2,18 +2,12 @@ package com.example.dacs
 
 import EpisodeAdapter
 import android.annotation.SuppressLint
-import android.content.Context
 import android.content.Intent
-import android.content.pm.ActivityInfo
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Surface
 import android.view.View
-import android.view.ViewGroup
-import android.view.WindowManager
-import android.webkit.*
-import android.widget.FrameLayout
+import android.webkit.WebChromeClient
+import android.webkit.WebView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -35,8 +29,6 @@ class WatchMovie : AppCompatActivity() {
     private lateinit var db2: DatabaseReference
     private val episodes= mutableListOf<Episode>()
     private val comments= mutableListOf<Comment>()
-    private lateinit var webview: WebView
-    private var isFullScreen = false
     data class EpisodeResponse(
         val id : Int,
         val results : List<Episode>
@@ -46,7 +38,7 @@ class WatchMovie : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityWatchMovieBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        webview = findViewById(R.id.WebView)
+
         recyclerView = binding.rvmovieEpisode
         recyclerView.layoutManager = LinearLayoutManager(
             this,
@@ -60,18 +52,19 @@ class WatchMovie : AppCompatActivity() {
             false
         )
 
-//        val webview = binding.WebView
+        val webview = binding.WebView
 
         binding.movieName.text = intent.getStringExtra("movieTitle")
         binding.des.text = "Description:"
         binding.tvTapphim.text = "Episodes:"
         binding.tvBinhluan.text = "Comments:"
         binding.movieDescription.text = intent.getStringExtra("movieOverview")
-
         val id = intent.getIntExtra("movieId", 0).toString()
         val IDUser = intent.getStringExtra("id")
         val NameUser = intent.getStringExtra("name")
-        getComment(id)
+        if (IDUser != null) {
+            getComment(id, IDUser)
+        }
         db = FirebaseDatabase.getInstance().getReference("Episodes")
         db.orderByChild("IDPhim").equalTo(id).addValueEventListener(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
@@ -80,6 +73,7 @@ class WatchMovie : AppCompatActivity() {
                         for (ds in snapshot.children) {
                             val episode = ds.getValue(Episode::class.java)
                             episodes.add(episode!!)
+
                         }
                         episodes[0].Video?.let { playVideo(webview, it) }
                     }
@@ -89,9 +83,11 @@ class WatchMovie : AppCompatActivity() {
                             override fun onItemClick(position: Int) {
                                 episodes[position].Video?.let { playVideo(webview, it) }
                             }
+
                         }
                     )
                     recyclerView.adapter = episodeAdapter
+
                 }
 
                 override fun onCancelled(error: DatabaseError) {
@@ -111,7 +107,9 @@ class WatchMovie : AppCompatActivity() {
                 .addOnFailureListener {
                     Toast.makeText(this, "Không thể bình luận", Toast.LENGTH_SHORT).show()
                 }
-            getComment(id)
+            if (IDUser != null) {
+                getComment(id, IDUser)
+            }
 
         }
 
@@ -119,89 +117,16 @@ class WatchMovie : AppCompatActivity() {
     @SuppressLint("SetJavaScriptEnabled")
     fun playVideo(webview: WebView, id: String) {
         val html = id
-
-//        webview.settings.javaScriptEnabled = true
-//        webview.settings.loadWithOverviewMode = true
-//        webview.settings.useWideViewPort = true
-//        webview.settings.builtInZoomControls = true
-//        webview.settings.displayZoomControls = false
-//        webview.webChromeClient = WebChromeClient()
-//        webview.loadUrl(html)
-
-        val webSettings = webview.settings
-        webSettings.javaScriptEnabled = true
-        webSettings.allowFileAccess = true
-        webSettings.allowContentAccess = true
-        webSettings.domStorageEnabled = true
-        webSettings.useWideViewPort = true
-        webSettings.loadWithOverviewMode = true
-        webSettings.setSupportMultipleWindows(true) // Cho phép chế độ toàn màn hình
-        webSettings.builtInZoomControls = true
-        webSettings.setSupportZoom(true)
-        webSettings.displayZoomControls = false
-        webSettings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
-        webSettings.setGeolocationEnabled(true)
-
-        webview.webChromeClient = object : WebChromeClient() {
-            override fun onShowCustomView(view: View?, callback: CustomViewCallback?) {
-                super.onShowCustomView(view, callback)
-                // Chuyển đổi video sang chế độ toàn màn hình
-                val videoView = view as FrameLayout
-                val decorView = window.decorView as FrameLayout
-                decorView.addView(
-                    videoView,
-                    ViewGroup.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.MATCH_PARENT
-                    )
-                )
-                webview.visibility = View.GONE
-                isFullScreen = true
-                requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE
-            }
-
-            override fun onHideCustomView() {
-                super.onHideCustomView()
-                // Thoát chế độ toàn màn hình
-                val decorView = window.decorView as FrameLayout
-                decorView.removeViewAt(decorView.childCount - 1)
-                webview.visibility = View.VISIBLE
-                isFullScreen = false
-                requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT
-            }
-        }
-
-        webview.webViewClient = object : WebViewClient() {
-            override fun onPageFinished(view: WebView?, url: String?) {
-                super.onPageFinished(view, url)
-                // Cập nhật hướng hiển thị hiện tại của màn hình
-                val display =
-                    (getSystemService(Context.WINDOW_SERVICE) as WindowManager).defaultDisplay
-                val orientation = when (display.rotation) {
-                    Surface.ROTATION_0 -> ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-                    Surface.ROTATION_90 -> ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-                    Surface.ROTATION_180 -> ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT
-                    else ->
-                        ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE
-                }
-                requestedOrientation = orientation
-            }
-            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
-                // Bắt sự kiện khi click vào link
-                val url = request?.url.toString()
-                if (url.endsWith(".mp4")) {
-                    val intent = Intent(Intent.ACTION_VIEW)
-                    intent.setDataAndType(Uri.parse(url), "video/*")
-                    startActivity(intent)
-                    return true
-                }
-                return false
-            }
-        }
+        webview.settings.javaScriptEnabled = true
+        webview.settings.loadWithOverviewMode = true
+        webview.settings.useWideViewPort = true
+        webview.settings.builtInZoomControls = true
+        webview.settings.displayZoomControls = false
+        webview.webChromeClient = WebChromeClient()
         webview.loadUrl(html)
-    }
 
-    private fun getComment(id: String) {
+    }
+    private fun getComment(id: String, idUser:String) {
 
         db2 = FirebaseDatabase.getInstance().getReference("Comments")
         db2.orderByChild("idphim").equalTo(id).addValueEventListener(object : ValueEventListener {
@@ -214,7 +139,7 @@ class WatchMovie : AppCompatActivity() {
                         val cmt = CmtSnap.getValue(Comment::class.java)
                         comments.add(cmt!!)
                     }
-                    commentAdapter = CommentAdapter(comments)
+                    commentAdapter = CommentAdapter(comments, idUser)
                     recyclerView1.adapter = commentAdapter
 
                 }
@@ -225,15 +150,4 @@ class WatchMovie : AppCompatActivity() {
             }
         })
     }
-
-    override fun onBackPressed() {
-        // Thoát chế độ toàn màn hình khi bấm nút back
-        if (isFullScreen) {
-            webview.stopLoading()
-            webview.goBack()
-        } else {
-            super.onBackPressed()
-        }
-    }
-
 }
